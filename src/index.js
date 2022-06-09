8ae3f116e8575ded15b63ef51a86fd1df1b36659
joshye <joshye@tencent.com>
2022-06-09 11:47:04 +0800

    feat: add entry for project

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
-import Notes from './data.csv';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  
-  element.append(myIcon);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  console.log(Data);
-  console.log(Notes);
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
   module: {
     rules: [
       {
